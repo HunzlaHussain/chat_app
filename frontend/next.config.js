431dcc5fb531f0e1bcf39b1c7eb5743f7fa9a6c6
HunzlaHussain <hunzlahussain7869@gmail.com>
2025-08-31 16:39:30 +0500

    feat: Add ESLint configuration to ignore errors during builds

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,7 +10,13 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
+
+
+module.exports = nextConfig
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
